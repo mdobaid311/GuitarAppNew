28cb0146beeadf98b8da62afc8f5031951636975
mujtaba.m <mujtaba.m@xitsolutionsinc.com>
2023-04-06 04:31:43 +0500

    BasicDashboardCharts

--- src/app/components/bar-chart/bar-chart.component.ts
+++ src/app/components/bar-chart/bar-chart.component.ts
@@ -16,36 +16,119 @@ export class BarChartComponent {
 
     this.chartOptions = {
       chart: {
-        type: 'bar'
-      },
-      title: {
-        text: this.options.text
-      },
-      xAxis: {
-        categories: this.options.categories
-      },
-      yAxis: {
-        min: 0,
-        title: {
-          text: 'Goals'
-        }
-      },
-      legend: {
-        layout: 'vertical', // changed to vertical layout
-        align: 'right', // moved to the right
-        verticalAlign: 'top', // moved to the top
-        x: -10, // adjust x position to align with chart
-        reversed: true
-      },
-      plotOptions: {
-        series: {
-          stacking: 'normal',
-          dataLabels: {
-            enabled: true
-          }
-        }
-      },
-      series: this.options.series
+  type: 'column'
+},
+title: {
+  text: 'Sales'
+},
+// subtitle: {
+//   text: 'Source: <a href="https://worldpopulationreview.com/world-cities" target="_blank">World Population Review</a>'
+// },
+xAxis: {
+  type: 'category',
+  labels: {
+    rotation: 0,
+    style: {
+      fontSize: '13px',
+      fontFamily: 'Verdana, sans-serif'
+    }
+  }
+},
+yAxis: {
+  // min: 0,
+  // title: {
+  //   text: 'Population (millions)'
+  // }
+  labels: {
+    enabled: false
+  },
+  title: {
+    text: null // Hide y-axis title
+  },
+  axisLabel: {
+    text: '' // Hide "Values" label
+  },
+  min: 0
+},
+legend: {
+  enabled: false
+},
+tooltip: {
+  pointFormat: 'Sales: <b>{point.y:.1f} millions</b>'
+},
+series: [{
+  name: 'Population',
+  data: [
+
+    ['12:00', 27.79],
+    ['1:00', 22.23],
+    ['2:00', 21.91],
+    ['3:00', 21.74],
+    ['4:00', 21.32],
+    ['5:00', 20.89],
+    ['6:00', 20.67],
+    ['7:00', 19.11],
+    ['8:00', 16.45],
+    ['9:00', 16.38],
+    ['10:00', 15.41],
+    ['11:00', 15.25],
+    ['12:00', 14.974],
+    ['1:00', 14.970],
+    ['2:00', 14.86],
+    ['3:00', 14.16],
+    ['4:00', 13.79],
+    ['5:00', 13.64],
+
+
+
+  ],
+  dataLabels: {
+    enabled: false // Remove data labels from columns
+    // enabled: true,
+    // rotation: -90,
+    // color: '#FFFFFF',
+    // align: 'right',
+    // format: '{point.y:.1f}', // one decimal
+    // y: 10, // 10 pixels down from the top
+    // style: {
+    //   fontSize: '13px',
+    //   fontFamily: 'Verdana, sans-serif'
+    // }
+  },
+  color: '#b39ddb', // Change color of columns
+  pointWidth: 10 // Reduce width of columns
+}]
+      // chart: {
+      //   type: 'bar'
+      // },
+      // title: {
+      //   text: this.options.text
+      // },
+      // xAxis: {
+      //   categories: this.options.categories
+      // },
+      // yAxis: {
+      //   min: 0,
+      //   title: {
+      //     text: 'Goals'
+      //   }
+      // },
+      // legend: {
+      //   layout: 'vertical', // changed to vertical layout
+      //   align: 'right', // moved to the right
+      //   verticalAlign: 'top', // moved to the top
+      //   x: -10, // adjust x position to align with chart
+      //   reversed: true
+      // },
+      // plotOptions: {
+      //   series: {
+      //     stacking: 'normal',
+      //     dataLabels: {
+      //       enabled: true
+      //     }
+      //   }
+      // },
+      // series: this.options.series
     };
   }
 
--- src/app/components/column-chart/column-chart.component.ts
+++ src/app/components/column-chart/column-chart.component.ts
@@ -14,66 +14,88 @@ export class ColumnChartComponent {
 
     this.chartOptions = {
       chart: {
-        type: 'column'
+        type: 'column',
       },
       title: {
-        text: 'Major trophies for some English teams',
-        align: 'left'
+        text: 'By Brand'
+      },
+      subtitle: {
+        // text: 'Source: <a href="https://worldpopulationreview.com/world-cities" target="_blank">World Population Review</a>'
       },
       xAxis: {
-        categories: ['Arsenal', 'Chelsea', 'Liverpool', 'Manchester United']
+        type: 'category',
+        labels: {
+          rotation: 270,
+          style: {
+            fontSize: '13px',
+            fontFamily: 'Verdana, sans-serif'
+          }
+        }
       },
       yAxis: {
         min: 0,
         title: {
-          text: 'Count trophies'
-        },
-        stackLabels: {
-          enabled: true,
-          style: {
-            fontWeight: 'bold',
-            color: ( // theme
-              Highcharts?.defaultOptions?.title?.style &&
-              Highcharts.defaultOptions.title.style.color
-            ) || 'gray',
-            textOutline: 'none'
-          }
+          text: 'Dollars in 1000' + "'" + "s"
         }
       },
       legend: {
-        align: 'left',
-        x: 70,
-        verticalAlign: 'top',
-        y: 70,
-        floating: true,
-        backgroundColor:
-          Highcharts?.defaultOptions?.legend?.backgroundColor || 'white',
-        borderColor: '#CCC',
-        borderWidth: 1,
-        shadow: false
+        enabled: false
       },
       tooltip: {
-        headerFormat: '<b>{point.x}</b><br/>',
-        pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+        pointFormat: 'Sales: <b>{point.y:.1f} millions</b>'
       },
       plotOptions: {
         column: {
-          stacking: 'normal',
+          pointPadding: 0.2,
+          borderWidth: 0,
+          colorByPoint: true,
           dataLabels: {
-            enabled: true
+            enabled: false
           }
         }
       },
       series: [{
-        name: 'BPL',
-        data: [3, 5, 1, 13]
-      }, {
-        name: 'FA Cup',
-        data: [14, 8, 8, 12]
-      }, {
-        name: 'CL',
-        data: [0, 2, 6, 3]
+        name: 'Population',
+        colorByPoint: true,
+        data: [
+          ['Tokyo', 37.33],
+          ['Delhi', 31.18],
+          ['Shanghai', 27.79],
+          ['Sao Paulo', 22.23],
+          ['Mexico City', 21.91],
+          ['Dhaka', 21.74],
+          ['Cairo', 21.32],
+          ['Beijing', 20.89],
+          ['Mumbai', 20.67],
+          ['Osaka', 19.11],
+          ['Karachi', 16.45],
+          ['Chongqing', 16.38],
+          ['Istanbul', 15.41],
+          ['Buenos Aires', 15.25],
+          ['Kolkata', 14.974],
+          ['Kinshasa', 14.970],
+          ['Lagos', 14.86],
+          ['Manila', 14.16],
+          ['Tianjin', 13.79],
+          ['Guangzhou', 13.64],
+          ['Istanbul', 15.41],
+          ['Buenos Aires', 15.25],
+          ['Kolkata', 14.974],
+          ['Kinshasa', 14.970],
+          ['Lagos', 14.86],
+          ['Manila', 14.16],
+          ['Tianjin', 13.79],
+          ['Guangzhou', 13.64],
+
+        ],
+        dataLabels: {
+          enabled: false,
+
+        },
+
+        pointWidth: 20 // Reduce width of columns
       }]
+
     };
   }
 
@@ -81,3 +103,154 @@ export class ColumnChartComponent {
     Highcharts.chart(this.chartContainer.nativeElement, this.chartOptions);
   }
 }
+
+
+// column single color
+
+// chart: {
+//   type: 'column'
+// },
+// title: {
+//   text: 'Sales'
+// },
+// // subtitle: {
+// //   text: 'Source: <a href="https://worldpopulationreview.com/world-cities" target="_blank">World Population Review</a>'
+// // },
+// xAxis: {
+//   type: 'category',
+//   labels: {
+//     rotation: 0,
+//     style: {
+//       fontSize: '13px',
+//       fontFamily: 'Verdana, sans-serif'
+//     }
+//   }
+// },
+// yAxis: {
+//   // min: 0,
+//   // title: {
+//   //   text: 'Population (millions)'
+//   // }
+//   labels: {
+//     enabled: false
+//   },
+//   title: {
+//     text: null // Hide y-axis title
+//   },
+//   axisLabel: {
+//     text: '' // Hide "Values" label
+//   },
+//   min: 0
+// },
+// legend: {
+//   enabled: false
+// },
+// tooltip: {
+//   pointFormat: 'Sales: <b>{point.y:.1f} millions</b>'
+// },
+// series: [{
+//   name: 'Population',
+//   data: [
+
+//     ['12:00', 27.79],
+//     ['1:00', 22.23],
+//     ['2:00', 21.91],
+//     ['3:00', 21.74],
+//     ['4:00', 21.32],
+//     ['5:00', 20.89],
+//     ['6:00', 20.67],
+//     ['7:00', 19.11],
+//     ['8:00', 16.45],
+//     ['9:00', 16.38],
+//     ['10:00', 15.41],
+//     ['11:00', 15.25],
+//     ['12:00', 14.974],
+//     ['1:00', 14.970],
+//     ['2:00', 14.86],
+//     ['3:00', 14.16],
+//     ['4:00', 13.79],
+//     ['5:00', 13.64],
+
+
+
+//   ],
+//   dataLabels: {
+//     enabled: false // Remove data labels from columns
+//     // enabled: true,
+//     // rotation: -90,
+//     // color: '#FFFFFF',
+//     // align: 'right',
+//     // format: '{point.y:.1f}', // one decimal
+//     // y: 10, // 10 pixels down from the top
+//     // style: {
+//     //   fontSize: '13px',
+//     //   fontFamily: 'Verdana, sans-serif'
+//     // }
+//   },
+//   color: '#b39ddb', // Change color of columns
+//   pointWidth: 10 // Reduce width of columns
+// }]
+
+// stacked column
+
+// chart: {
+//   type: 'column'
+// },
+// title: {
+//   text: 'Major trophies for some English teams',
+//   align: 'left'
+// },
+// xAxis: {
+//   categories: ['Arsenal', 'Chelsea', 'Liverpool', 'Manchester United']
+// },
+// yAxis: {
+//   min: 0,
+//   title: {
+//     text: 'Count trophies'
+//   },
+//   stackLabels: {
+//     enabled: true,
+//     style: {
+//       fontWeight: 'bold',
+//       color: ( // theme
+//         Highcharts?.defaultOptions?.title?.style &&
+//         Highcharts.defaultOptions.title.style.color
+//       ) || 'gray',
+//       textOutline: 'none'
+//     }
+//   }
+// },
+// legend: {
+//   align: 'left',
+//   x: 70,
+//   verticalAlign: 'top',
+//   y: 70,
+//   floating: true,
+//   backgroundColor:
+//     Highcharts?.defaultOptions?.legend?.backgroundColor || 'white',
+//   borderColor: '#CCC',
+//   borderWidth: 1,
+//   shadow: false
+// },
+// tooltip: {
+//   headerFormat: '<b>{point.x}</b><br/>',
+//   pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+// },
+// plotOptions: {
+//   column: {
+//     stacking: 'normal',
+//     dataLabels: {
+//       enabled: true
+//     }
+//   }
+// },
+// series: [{
+//   name: 'BPL',
+//   data: [3, 5, 1, 13]
+// }, {
+//   name: 'FA Cup',
+//   data: [14, 8, 8, 12]
+// }, {
+//   name: 'CL',
+//   data: [0, 2, 6, 3]
+// }]
