e5a7a2324c6dfa097e8a49cd4e94d22d91024e8a
mujtaba.m <mujtaba.m@xitsolutionsinc.com>
2023-04-10 21:58:58 +0500

    columnSelectable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AreaSplineChartComponent } from './components/area-spline-chart/area-spline-chart.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'dashboard', component: DashboardComponent},
+  {path: 'area-chart', component: AreaSplineChartComponent},
+
 ];
 
 @NgModule({
