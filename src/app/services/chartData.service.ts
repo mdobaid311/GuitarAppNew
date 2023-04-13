0d124f6e78f33bd1a9e515ef0edfea00bc320274
mujtaba.m <mujtaba.m@xitsolutionsinc.com>
2023-04-14 00:39:29 +0500

    Orgchartngprime2

--- src/app/services/chartData.service.ts
+++ src/app/services/chartData.service.ts
@@ -14,22 +14,20 @@ export class ChartService {
 
   constructor(private httpClient: HttpClient,private httpReqService: HttpRequestService,private router: Router) { }
 
-  // public getAllChartData(usertoken:any) {
 
-  //  return this.httpReqService.request({
-  //    method: APP_CONSTANTS.API_METHODS.GET,
-  //    url: environment.SERVICE_APIS.LIST_ALL_PACKAGE_METHOD,
-  //    headerConfig: {token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2IyYzE2MWI3OGFjYTI3NGMyMzU2NWMiLCJmaXJzdG5hbWUiOiJBbWphZCIsImxhc3RuYW1lIjoiaXFiYWwiLCJlbWFpbCI6ImFtamFkLmlxYmFsQGFjcnV4c3lzdGVtcy5jby51ayIsInJvbGUiOiJhY2NvdW50T3duZXIiLCJjcmVhdGVkQXQiOiIyMDIzLTAxLTAyVDExOjM0OjU4LjIzMFoiLCJpYXQiOjE2ODA2NDE2OTZ9.p2M-yVFyGFH4bXodLkOhhhl6MhInItvOaWxGI5YT6l8'}
-  //   })
-  // }
 
-  public getData(): Observable<any[]>  {
-    const requestBody = {
-      columns:["INVOICED_LINE_TOTAL"]
-    };
-
-    return this.httpClient.post<any[]>(environment.SERVICE_APIS.query + 'include', requestBody)
+  public getData(year:any) {
+    const url = `${environment.SERVICE_APIS.ORIGINAL_TOTAL}?year=${year}`;
+           return this.httpClient.get<any[]>(url);
   }
+
+  // public getData(): Observable<any[]>  {
+  //   const requestBody = {
+  //     columns:["INVOICED_LINE_TOTAL"]
+  //   };
+
+  //   return this.httpClient.post<any[]>(environment.SERVICE_APIS.ORIGINAL_TOTAL + 'include', requestBody)
+  // }
 }
 
 
