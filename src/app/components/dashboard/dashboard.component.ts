32f75aed6a47655f7f85489cf36aee0b10c0d41e
mdobaid311 <mdobaid311@gmail.com>
2023-04-14 00:57:25 +0530

    new commit

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,42 +1,71 @@
 import { Component, OnInit } from '@angular/core';
-import { ChartService } from '../../services/chartData.service'
+import { ChartService } from '../../services/chartData.service';
 import { Router } from '@angular/router';
+import { faSortDown, faSortUp } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit {
+  constructor(private chartData: ChartService, private router: Router) {}
 
-  dataRows = []
+  faSortDown = faSortDown;
+  faSortUp = faSortUp;
 
-  constructor(private chartData: ChartService, private router: Router){}
+  dataRows = [];
+
+  percentChange = 20;
+
+  selectedYear = '';
+  selectedMonth = '';
+  selectedDay = '';
+
+  onSelectYearChange(event: any) {
+    this.selectedYear = event.target.value;
+    console.log('selectedYear', this.selectedYear);
+  }
+
+  onSelectMonthChange(event: any) {
+    this.selectedMonth = event.target.value;
+    console.log('selectedMonth', this.selectedMonth);
+  }
+
+  onSelectDayChange(event: any) {
+    this.selectedDay = event.target.value;
+    console.log('selectedDay', this.selectedDay);
+  }
 
   ngOnInit(): void {
-      this.chartData.getData().subscribe({
-        next: (resp:any) => {
-          this.dataRows = resp.rows
-          console.log('ROWS', this.dataRows)
-        }
-      })
+    this.chartData.getData().subscribe({
+      next: (resp: any) => {
+        this.dataRows = resp.rows;
+        console.log('ROWS', this.dataRows);
+      },
+    });
   }
 
   barChartOptions = {
-    text: "New Bar Chart 1",
+    text: 'New Bar Chart 1',
     categories: ['2020/21', '2019/20', '2018/19', '2017/18', '2016/17'],
-    series: [{
-      name: 'Cristiano Ronaldo',
-      data: [4, 4, 6, 15, 12]
-    }, {
-      name: 'Lionel Messi',
-      data: [5, 3, 12, 6, 11]
-    }, {
-      name: 'Kaka',
-      data: [5, 3, 12, 6, 12]
-    }, {
-      name: 'Robert Lewandowski',
-      data: [5, 15, 8, 5, 8]
-    }]
-  }
+    series: [
+      {
+        name: 'Cristiano Ronaldo',
+        data: [4, 4, 6, 15, 12],
+      },
+      {
+        name: 'Lionel Messi',
+        data: [5, 3, 12, 6, 11],
+      },
+      {
+        name: 'Kaka',
+        data: [5, 3, 12, 6, 12],
+      },
+      {
+        name: 'Robert Lewandowski',
+        data: [5, 15, 8, 5, 8],
+      },
+    ],
+  };
 }
