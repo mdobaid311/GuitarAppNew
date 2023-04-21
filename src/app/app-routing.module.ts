c66ab1f35354276c4e4c26df12b7c4efc06449c7
mujtaba.m <mujtaba.m@xitsolutionsinc.com>
2023-04-21 19:30:59 +0500

    newSalescomponentAdded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AreaSplineChartComponent } from './components/area-spline-chart/area-spline-chart.component';
 import { OrgChartComponent } from './components/org-chart/org-chart.component';
+import { SalesComponent } from './components/sales/sales.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'area-chart', component: AreaSplineChartComponent},
   {path: 'org-chart', component: OrgChartComponent},
+  {path: 'sales', component: SalesComponent},
 
 ];
 
