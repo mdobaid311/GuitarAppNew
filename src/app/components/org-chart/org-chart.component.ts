d357bb1e538bc32ffeab1d96c8edd1f6c2a80ce6
mdobaid311 <mdobaid311@gmail.com>
2023-04-25 16:13:17 +0530

    org chart updated

--- src/app/components/org-chart/org-chart.component.ts
+++ src/app/components/org-chart/org-chart.component.ts
@@ -1,68 +1,55 @@
+import { map } from 'rxjs/operators';
 import { Component } from '@angular/core';
+import { ChartService } from 'src/app/services/chartData.service';
 
 @Component({
   selector: 'app-org-chart',
   templateUrl: './org-chart.component.html',
-  styleUrls: ['./org-chart.component.scss']
+  styleUrls: ['./org-chart.component.scss'],
 })
 export class OrgChartComponent {
+  data: any;
+  constructor(private chartData: ChartService) {}
 
-  data = [
-    {
-      label: 'CEO',
-      type: 'person',
-      styleClass: 'chart_node',
-      expanded: true,
-      data: { name: 'John Doe', title: 'CEO', gender: 'male', image: 'https://randomuser.me/api/portraits/men/1.jpg' },
-      children: [
-        {
-          label: 'CFO',
-          type: 'person',
-          styleClass: 'chart_node_1',
-          expanded: true,
-          data: { name: 'Jane Smith', title: 'CFO', gender: 'female', image: 'https://randomuser.me/api/portraits/women/1.jpg' },
-          children: [
-            {
-              label: 'Accountant',
-              type: 'person',
-              styleClass: 'chart_node_2',
-              expanded: true,
-              data: { name: 'Bob Johnson', title: 'Accountant', gender: 'male', image: 'https://randomuser.me/api/portraits/men/2.jpg' }
+  ngOnInit() {
+    this.chartData.getOrgChartData().subscribe({
+      next: (resp: any) => {
+        console.log(resp);
+        this.data = [
+          {
+            label: 'Sales',
+            type: 'person',
+            styleClass: 'chart_node',
+            expanded: true,
+            data: {
+              name: 'Sales',
+              title: 'Sales',
+              gender: 'male',
             },
-            {
-              label: 'Finance Manager',
-              type: 'person',
-              styleClass: 'chart_node_2',
-              expanded: true,
-              data: { name: 'Mary Brown', title: 'Finance Manager', gender: 'female', image: 'https://randomuser.me/api/portraits/women/2.jpg' }
-            }
-          ]
+            children: this.generateNode(resp),
+          },
+        ];
+      },
+    });
+  }
+
+  generateNode(children: any) {
+    return children.map((node: any) => {
+      return {
+        label: node.key || 'Others',
+        type: 'person',
+        styleClass: 'chart_node',
+        expanded: true,
+        data: {
+          title: node.key || 'Others',
         },
-        {
-          label: 'CTO',
-          type: 'person',
-          styleClass: 'chart_node_1',
-          expanded: true,
-          data: { name: 'Mark Lee', title: 'CTO', gender: 'male', image: 'https://randomuser.me/api/portraits/men/3.jpg' },
-          children: [
-            {
-              label: 'Software Engineer',
-              type: 'person',
-              styleClass: 'chart_node_2',
-              expanded: true,
-              data: { name: 'Tom Wilson', title: 'Software Engineer', gender: 'male', image: 'https://randomuser.me/api/portraits/men/4.jpg' }
-            },
-            {
-              label: 'QA Engineer',
-              type: 'person',
-              styleClass: 'chart_node_2',
-              expanded: true,
-              data: { name: 'Lisa Chen', title: 'QA Engineer', gender: 'female', image: 'https://randomuser.me/api/portraits/women/3.jpg' }
-            }
-          ]
-        }
-      ]
-    }
-  ];
+        children:
+          node.children.length > 0 ? this.generateNode(node.children) : [],
+      };
+    });
+  }
 
+  expandNode(event: any) {
+    console.log(event);
+  }
 }
