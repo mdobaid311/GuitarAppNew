6e0add3f0c08b50169254fe769ba54b889dc7fb9
mdobaid311 <mdobaid311@gmail.com>
2023-05-01 19:55:35 +0530

    pie chart fixed

--- src/app/components/column-chart/column-chart.component.ts
+++ src/app/components/column-chart/column-chart.component.ts
@@ -1,4 +1,12 @@
-import { Component, ElementRef, Input, SimpleChanges, ViewChild, OnInit, OnDestroy } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  SimpleChanges,
+  ViewChild,
+  OnInit,
+  OnDestroy,
+} from '@angular/core';
 import * as Highcharts from 'highcharts';
 import { ChartService } from 'src/app/services/chartData.service';
 import { Subscription } from 'rxjs';
@@ -13,8 +21,8 @@ export class ColumnChartComponent {
 
   chartOptions: any;
 
-  newDataArray:any = [];
-  subscription: Subscription = new Subscription;
+  newDataArray: any = [];
+  subscription: Subscription = new Subscription();
 
   theme = 'light';
   loader = false;
@@ -33,19 +41,15 @@ export class ColumnChartComponent {
     observer.observe(document.body, { attributes: true });
   }
 
-
-
   ngOnInit() {
-
-    this.subscription = this.chartData.dataArray.subscribe(array => {
-      console.log('Array', array)
+    this.subscription = this.chartData.dataArray.subscribe((array) => {
+      console.log('Array', array);
       this.newDataArray = array;
 
       this.chartOptions = {
         chart: {
           type: 'column',
           height: (9 / 16) * 40 + '%',
-
         },
 
         title: {
@@ -92,37 +96,31 @@ export class ColumnChartComponent {
           {
             name: 'Population',
             data: array,
-
             dataLabels: {
-              enabled: false, // Remove data labels from columns
-
+              enabled: true, // Remove data labels from columns
             },
             color: '#2f7ed8', // Change color of columns
             pointWidth: 25, // Reduce width of columns
             backgroundColor: '#FCFFC5',
           },
         ],
-
       };
       Highcharts.chart(this.chartContainer.nativeElement, this.chartOptions);
       this.updateChartTheme();
-    })
+    });
     this.loadInitialchart();
   }
 
   loadInitialchart() {
     this.loader = true;
     this.chartData.getOrderTotalYears().subscribe({
-      next: resp => {
-        let yearsData:any = [];
-        console.log('Year data', resp)
-        resp.forEach((item:IYear) => {
-          const itemData = [
-            item.year,
-            item.total,
-          ];
-            yearsData.push(itemData);
-        })
+      next: (resp) => {
+        let yearsData: any = [];
+        console.log('Year data', resp);
+        resp.forEach((item: IYear) => {
+          const itemData = [item.year, item.total];
+          yearsData.push(itemData);
+        });
         this.chartOptions = {
           chart: {
             type: 'column',
@@ -143,7 +141,6 @@ export class ColumnChartComponent {
               rotation: 0,
               style: {
                 color: '#000',
-
                 fontFamily: 'Verdana, sans-serif',
               },
             },
@@ -166,6 +163,7 @@ export class ColumnChartComponent {
             title: {
               text: 'Dollars in 1000' + "'" + 's',
             },
+
             labels: {
               rotation: 0,
               style: {
@@ -188,8 +186,8 @@ export class ColumnChartComponent {
               data: yearsData,
 
               dataLabels: {
-                enabled: false, // Remove data labels from columns
-
+                enabled: true, // Remove data labels from columns
+                color:"#fff"
               },
               color: '#2f7ed8', // Change color of columns
               pointWidth: 25, // Reduce width of columns
@@ -199,23 +197,20 @@ export class ColumnChartComponent {
           plotOptions: {
             column: {
               events: {
-                click: function (event:any) {
-                  const name = +event.point.name
+                click: function (event: any) {
+                  const name = +event.point.name;
                   alert('Value of clicked column: ' + name);
                 },
               },
             },
           },
-
         };
         Highcharts.chart(this.chartContainer.nativeElement, this.chartOptions);
         this.updateChartTheme();
         this.loader = false;
       },
-      error: error => {
-
-      }
-    })
+      error: (error) => {},
+    });
   }
 
   updateChartTheme() {
@@ -227,7 +222,7 @@ export class ColumnChartComponent {
       this.theme === 'dark' ? '#19376D' : '#fff';
     this.chartOptions.xAxis.labels.style.color =
       this.theme === 'dark' ? '#fff' : '#000';
-      this.chartOptions.yAxis.labels.style.color =
+    this.chartOptions.yAxis.labels.style.color =
       this.theme === 'dark' ? '#fff' : '#000';
     this.chartOptions.title.style.color =
       this.theme === 'dark' ? '#fff' : '#000';
@@ -244,8 +239,6 @@ export class ColumnChartComponent {
 }
 
 class IYear {
-  "year": string;
-  "total": number;
+  'year': string;
+  'total': number;
 }
-
-
