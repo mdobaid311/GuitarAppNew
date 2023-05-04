89f50abd6b046f76a9942b23cf1e902a94d513a1
mdobaid311 <mdobaid311@gmail.com>
2023-05-04 15:34:06 +0530

    added custom loader

---
+++ src/app/components/custom-guitar-loader/custom-guitar-loader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-guitar-loader',
+  templateUrl: './custom-guitar-loader.component.html',
+  styleUrls: ['./custom-guitar-loader.component.scss']
+})
+export class CustomGuitarLoaderComponent {
+
+}
