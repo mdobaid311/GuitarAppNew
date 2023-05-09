051c8574337a607f2998a62695a833a728cc3ba9
mdobaid311 <mdobaid311@gmail.com>
2023-05-09 14:21:30 +0530

    folder str updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { AreaSplineChartComponent } from './components/area-spline-chart/area-spline-chart.component';
-import { OrgChartComponent } from './components/org-chart/org-chart.component';
-import { SalesComponent } from './components/sales/sales.component';
+import { AreaSplineChartComponent } from './components/charts/area-spline-chart/area-spline-chart.component';
+import { OrgChartComponent } from './components/charts/org-chart/org-chart.component';
+import { SalesComponent } from './components/sales-module/sales/sales.component';
 import { SettingsComponent } from './components/settings/settings.component';
-import { Orgchart2Component } from './components/orgchart2/orgchart2.component';
+import { Orgchart2Component } from './components/charts/orgchart2/orgchart2.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
