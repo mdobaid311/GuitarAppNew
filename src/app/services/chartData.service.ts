ae452a886c153bf892a3b03b18e5c8b515831c19
mdobaid311 <mdobaid311@gmail.com>
2023-05-22 20:35:32 +0530

    new commit

--- src/app/services/chartData.service.ts
+++ src/app/services/chartData.service.ts
@@ -108,16 +108,16 @@ export class ChartService {
   }
 
   public getFullSalesData(startDate: any, endDate: any, intervaltime: any) {
-    const url = `http://localhost:3000/v2/alsd/get-full-sales-data?start_date=${startDate}&end_date=${endDate}&intervaltime=${intervaltime}`;
+    const url = `http://3.111.38.149:3000/v2/alsd/get-full-sales-data?start_date=${startDate}&end_date=${endDate}&intervaltime=${intervaltime}`;
     return this.httpClient.get<any[]>(url);
   }
   public getFullSalesDataByRange(startDate: any, endDate: any, intervaltime: any) {
-    const url = `http://localhost:3000/v2/tables/getFullSalesData?start_date=${startDate}&end_date=${endDate}&intervaltime=${intervaltime}`;
+    const url = `http://3.111.38.149:3000/v2/tables/getFullSalesData?start_date=${startDate}&end_date=${endDate}&intervaltime=${intervaltime}`;
     return this.httpClient.get<any[]>(url);
   }
 
   public getTableData(tableName: any) {
-    const url = `http://localhost:3000/v2/tables?table=${tableName}`;
+    const url = `http://3.111.38.149:3000/v2/tables?table=${tableName}`;
     return this.httpClient.get<any[]>(url);
   }
 }
