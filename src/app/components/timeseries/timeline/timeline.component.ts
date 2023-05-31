7de27adbc1ffc67abff11e86abc1fd84743d1361
mdobaid311 <mdobaid311@gmail.com>
2023-05-31 18:50:27 +0530

    new commit

--- src/app/components/timeseries/timeline/timeline.component.ts
+++ src/app/components/timeseries/timeline/timeline.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { faClock} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
-  styleUrls: ['./timeline.component.scss']
+  styleUrls: ['./timeline.component.scss'],
 })
 export class TimelineComponent {
+  constructor() {}
 
+  faClock = faClock;
+
+  timelineSelectOptions = [
+    { name: '1', icon: faClock },
+    { name: '2', icon: faClock },
+    { name: '3', icon: faClock },
+    { name: '4', icon: faClock },
+    { name: '5', icon: faClock },
+  ];
 }
