a9247c805e503a986b1340d2c442959e3bc5949a
mdobaid311 <mdobaid311@gmail.com>
2023-06-01 22:43:44 +0530

    new commit

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { faUser, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { DatetimeService } from 'src/app/services/datetime.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-settings',
@@ -12,7 +13,51 @@ export class SettingsComponent {
   faPlus = faPlus;
   showCreateUserForm: boolean = false;
 
-  constructor(private datetime: DatetimeService) {}
+  constructor(
+    private datetime: DatetimeService,
+    private userService: UserService
+  ) {}
+
+  firstName: string = '';
+  lastName: string = '';
+  userName: string = '';
+  role: string = '';
+  password: string = '';
+
+  onChange(fieldName: string, event: any) {
+    switch (fieldName) {
+      case 'firstName':
+        this.firstName = event.target.value;
+        break;
+      case 'lastName':
+        this.lastName = event.target.value;
+        break;
+      case 'userName':
+        this.userName = event.target.value;
+        break;
+      case 'role':
+        this.role = event.target.value;
+        break;
+      case 'password':
+        this.password = event.target.value;
+        break;
+    }
+  }
+
+  onSubmit() {
+    console.log('submit');
+    const data = {
+      firstname: this.firstName,
+      lastname: this.lastName,
+      username: this.userName,
+      role: this.role,
+      password: this.password,
+    };
+    console.log(data)
+    this.userService.register(data).subscribe((res) => {
+      console.log(res);
+    });
+  }
 
   toggleCreateUserForm() {
     this.showCreateUserForm = !this.showCreateUserForm;
