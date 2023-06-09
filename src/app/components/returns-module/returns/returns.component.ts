381b38d524623213f1218ca7e43928574f2f1716
mdobaid311 <mdobaid311@gmail.com>
2023-06-09 23:50:22 +0530

    new commit

--- src/app/components/returns-module/returns/returns.component.ts
+++ src/app/components/returns-module/returns/returns.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ReturnsService } from 'src/app/services/returns.service';
+import data from "./data.json"
 
 @Component({
   selector: 'app-returns',
@@ -6,6 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./returns.component.scss'],
 })
 export class ReturnsComponent {
-
   loader = false;
+
+  constructor(private returnsServices: ReturnsService) {}
+
+  returnsData:any
+
+  ngOnInit(): void {
+    // this.loader = true;
+    // this.returnsServices.getReturnsData('2023-03-22').subscribe((res) => {
+    //   console.log(res);
+    //   this.returnsData = res;
+    //   this.loader = false;
+    // });
+
+    this.returnsData = data
+  }
 }
